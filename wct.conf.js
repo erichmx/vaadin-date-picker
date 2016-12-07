657e779288f4a0a18640e3877f92c2fa4c160fcd
Anton Platonov <anton@vaadin.com>
2016-12-07 13:21:04 +0200

    --wip--

--- wct.conf.js
+++ wct.conf.js
@@ -2,9 +2,6 @@
 
 module.exports = {
   registerHooks: function(context) {
-    context.emit('log:debug', '-- BROWSERS --');
-    context.emit('log:debug', process.env.BROWSERS);
-    context.emit('log:debug', JSON.stringify(JSON.parse(process.env.BROWSERS)));
-    context.options.plugins.sauce.browsers = JSON.parse(process.env.BROWSERS);
+    context.options.plugins.sauce.browsers = process.env.SAUCE_BROWSERS ? JSON.parse(process.env.SAUCE_BROWSERS) : 'default';
   }
 };
